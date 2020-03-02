3621c829c472404ba9b475aad9be4ed536b12552
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-03-02 16:07:31 +0000

    creates HomePage

---
+++ alwayscurious/src/containers/HomePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+
+            }
+        }
+    
+    render() {
+        return (
+            <h1>Mary Cummings</h1>
+        )
+    }
+    }
+
+
+
+    export default HomePage;
+
